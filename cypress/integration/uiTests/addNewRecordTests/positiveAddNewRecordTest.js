dd6c6084c35f35feb8c730677fdbf5098662a04c
Dmitrii Semenov <grogandwine@gmail.com>
2022-05-05 13:24:14 -0400

    Adding description

--- cypress/integration/uiTests/addNewRecordTests/positiveAddNewRecordTest.js
+++ cypress/integration/uiTests/addNewRecordTests/positiveAddNewRecordTest.js
@@ -6,10 +6,12 @@ import faker from "@faker-js/faker";
 
 describe('Positive test for "add new employee" feature', () => {
 
+    // Creating objects of PageObject classes
     const loginPage = new LoginPage();
     const dashBoardPage = new DashboardPage();
     const addNewEmployeePage = new AddNewEmployeePage();
 
+    // Creating data for form fields
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
     const dependantNum = faker.datatype.number({
@@ -18,11 +20,16 @@ describe('Positive test for "add new employee" feature', () => {
     });
 
     it('should run positive test for "add new employee" form %use after%',function () {
+
+        // Login to application using PageObject method
         cy.visit('/');
         loginPage.loginToApplication(Cypress.env('login'), Cypress.env('password'));
+        // Adding new record to the table
         dashBoardPage.clickOnAddEmployeeButton();
         addNewEmployeePage.addNewRecord(firstName, lastName, dependantNum).then(id => {
+            // Saving Record ID for AfterEach Delete Record method
             cy.wrap([id]).as('recordId');
+            // Asserting new record data via UI
             dashBoardPage.checkNewRecordOnUI(id, firstName, lastName, dependantNum);
         });
     });
--- cypress/integration/uiTests/updateRecordTests/positiveUpdateRecord.js
+++ cypress/integration/uiTests/updateRecordTests/positiveUpdateRecord.js
@@ -4,24 +4,30 @@ import faker from "@faker-js/faker";
 
 describe('Tests to update record', () => {
 
+    // Creating objects of PageObject classes
     const dashBoardPage = new DashboardPage();
     const updateRecordPage = new UpdateRecordPage();
 
+    // Creating new data for form fields
     const newFirstName = faker.name.firstName();
     const newLastName = faker.name.lastName();
     const newDependantNum = faker.datatype.number({
         'min': 0,
-        'max': 31
+        'max': 32
     });
 
     it('should create a new record via API and then update it via UI %use before% %use after%', function () {
 
         // Creating new record via API
         cy.get('@recordId').then(([id]) => {
+            // Login to application via UI
             cy.loginViaUi(Cypress.env('login'), Cypress.env('password'));
+            // Updating record
             dashBoardPage.clickUpdateRecordButton(id);
             updateRecordPage.updateRecord(newFirstName, newLastName, newDependantNum);
+            // Need to wait until record is updated on the page
             cy.wait(250);
+            // Asserting results
             dashBoardPage.checkNewRecordOnUI(id, newFirstName, newLastName, newDependantNum);
         });
     });
