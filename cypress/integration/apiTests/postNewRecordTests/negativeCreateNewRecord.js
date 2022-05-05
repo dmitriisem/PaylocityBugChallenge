3158420416c15fa863f634f1bbd5923b6a30adc8
Dmitrii Semenov <grogandwine@gmail.com>
2022-05-05 13:33:14 -0400

    Adding description

--- cypress/integration/apiTests/postNewRecordTests/negativeCreateNewRecord.js
+++ cypress/integration/apiTests/postNewRecordTests/negativeCreateNewRecord.js
@@ -2,20 +2,24 @@ import {testData} from '/cypress/integration/apiTests/postNewRecordTests/support
 
 describe('This is should be data driver API test', () => {
 
+    // Using forEach method for Data-Driven testing and loads data from negativeCreateNewRecData.json file
     testData.forEach(td => {
         it(`should test ${td.field} field in the "Add new employee" form`, function () {
 
+            // Data for request body
             const body = {
                 "firstName": td.firstName,
                 "lastName": td.lastName,
                 "dependants": td.dependants
             };
 
+            // Headers for request
             const headers = {
                 'Authorization': Cypress.env('token'),
                 'Content-Type': "application/json"
             };
 
+            // Sending POST request
             cy.request({
                 method: "POST",
                 url: Cypress.env('apiURL'),
@@ -23,6 +27,7 @@ describe('This is should be data driver API test', () => {
                 body: body,
                 failOnStatusCode: false
             }).then(response => {
+                // Asserting response
                 expect(response.status).to.eq(td.statusCode);
                 expect(response.body[0].errorMessage).to.eq(td.errorMessage);
             });
